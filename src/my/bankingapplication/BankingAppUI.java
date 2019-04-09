9d3a57b229221ff68e878a1d330c447ca0fbf490
NicolasGamez <38731864+NicolasGamez@users.noreply.github.com>
2019-04-09 14:00:01 -0500

    Revert "Merge branch 'master' of https://github.com/CristhianT93/BankingApplicationV2"
    This reverts commit ae95801ac5c9626739e66ff877d2a18a45d44938, reversing
    changes made to 2a4f6da53986d8437570208b245eadfb992e5c49.

--- src/my/bankingapplication/BankingAppUI.java
+++ src/my/bankingapplication/BankingAppUI.java
@@ -105,13 +105,11 @@ public class BankingAppUI extends javax.swing.JFrame {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             Connection con = DriverManager.getConnection("jdbc:sqlserver://oop-java.database.windows.net:1433;databaseName=Banking Database", "TeamRocket", "Giovanni3");
-
             String query = "select * from db_accessadmin.Employee where username=? and password=?";
             PreparedStatement ps = con.prepareStatement(query);
             ps.setString(1, txtUsername.getText());
             ps.setString(2, txtPassword.getText());
             ResultSet rs = ps.executeQuery();
-            
             if(rs.next()){
                 //JOptionPane.showMessageDialog(null, "YES");
                 CustomerSearch newFrame = new CustomerSearch();
